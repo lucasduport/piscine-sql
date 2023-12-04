700956ad24d1f2c87bb8290727552715cf23cd9e
Lucas Duport <lucas.duport@epita.fr>
2023-12-04 20:29:38 +0100

    socially-unfit: tag number 3

--- socially_unfit/req04.sql
+++ socially_unfit/req04.sql
@@ -1,10 +1,11 @@
 SELECT first_name, last_name, birth_date, job_id, social_credit_balance
 FROM public.people
 WHERE job_id IS NOT NULL
+AND death_date IS NULL
 AND social_credit_balance > 0
 ORDER BY
     social_credit_balance DESC,
     birth_date ASC,
     job_id ASC
 LIMIT 100
-OFFSET 224;
+OFFSET 200;
