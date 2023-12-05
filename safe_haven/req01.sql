c4374bd87303dfd2778805667a9e6a1953cdd55d
Lucas Duport <lucas.duport@epita.fr>
2023-12-05 23:53:48 +0100

    safe-haven: tag number 4

--- safe_haven/req01.sql
+++ safe_haven/req01.sql
@@ -3,7 +3,7 @@ CREATE TABLE banking_transactions_poc
     id serial PRIMARY KEY CONSTRAINT positive_id CHECK (id > 0),
     sender varchar(22) CONSTRAINT sneder_iban CHECK (sender SIMILAR TO 'RR[0-9]{2}[A-Z]{4}[0-9]{16}'),
     receiver varchar(22) CONSTRAINT receiver_iban CHECK (receiver SIMILAR TO 'RR[0-9]{2}[A-Z]{4}[0-9]{16}'),
-    amount numeric(16, 2) CONSTRAINT positive_amount CHECK (amount > 0.00) NOT NULL,
+    amount numeric(16, 2) CONSTRAINT positive_amount CHECK (amount > 0.000),
 
     CHECK (sender NOT SIMILAR TO receiver)
 );
