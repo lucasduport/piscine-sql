34ec1357857ea6da996ce579e065062b56ba8862
Lucas Duport <lucas.duport@epita.fr>
2023-12-07 21:52:17 +0100

    red-flag: tag number 4

--- red_flag/req02.sql
+++ red_flag/req02.sql
@@ -1,5 +1,7 @@
 UPDATE  public.epix_posts
 SET   downvotes = upvotes * ( 
-    (SELECT avg(ep.downvotes::numeric/NULLIF(ep.upvotes::numeric,0)) FROM  public.epix_posts as ep)
-/ (SELECT count(*) FILTER (WHERE ep.author_id = 933) FROM public.epix_posts as ep))
+    (SELECT 
+        (sum(ep.downvotes::numeric/ep.upvotes::numeric) / 3)
+        FROM  public.epix_posts as ep
+        WHERE author_id = 933 AND id <> 139))
 WHERE id = 139;
