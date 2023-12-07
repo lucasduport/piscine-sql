3605b91d0967a079ae52d2c68b28d5a73c9c9467
Lucas Duport <lucas.duport@epita.fr>
2023-12-07 23:59:07 +0100

    deprecated-happiness: tag number 5

--- deprecated_happiness/req04.sql
+++ deprecated_happiness/req04.sql
@@ -4,6 +4,6 @@ ADD FOREIGN KEY (person_id) REFERENCES public.people(id);
 
 INSERT INTO migration.patients_records
 SELECT DISTINCT pi.person_id
-FROM migration.patients_ids AS pi
+FROM medical_confidential.patients_ids AS pi
 JOIN migration.patients_records AS pr
 ON pr.id = pi.patient_id;
