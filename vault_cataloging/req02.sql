f111801db83143132196a9c75809d0f6ff2c55c0
Lucas Duport <lucas.duport@epita.fr>
2023-12-07 17:06:31 +0100

    vault-cataloging: tag number 4

--- vault_cataloging/req02.sql
+++ vault_cataloging/req02.sql
@@ -1,16 +1,17 @@
 SELECT
-    id,
-    size AS stored_size,
-    decrypted,
+    mfs.id,
+    mfs.size AS stored_size,
+    mfs.decrypted,
         CASE
-        WHEN size IS NULL AND decrypted = true THEN CAST((SELECT avg(size) 
-            FILTER (WHERE size IS NOT NULL and decrypted = true) 
+        WHEN mfs.size IS NULL AND mfs.decrypted = true THEN CAST((SELECT avg(size) 
+            FILTER (WHERE parent_id IS NOT NULL and decrypted = true) 
                                     FROM dtf.madelines_files_results as mf) AS bigint)
-        WHEN size IS NULL AND decrypted = false THEN CAST((SELECT avg(size) 
-            FILTER (WHERE size IS NOT NULL and decrypted = false) 
-                                    FROM dtf.madelines_files_results as mf) AS bigint)
+        WHEN mfs.size IS NULL AND decrypted = false THEN CAST((SELECT avg(size) 
+        FILTER (WHERE parent_id IS NOT NULL and decrypted = false) 
+                                FROM dtf.madelines_files_results) AS bigint)
 
-        ELSE size
+        ELSE mfs.size
         END AS calculated_size
 FROM dtf.madelines_files_results as mfs
-WHERE parent_id IS NOT NULL;
+WHERE parent_id IS NOT NULL
+ORDER BY id
