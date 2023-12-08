3964ab6cca3b90484e4d461adb8404ec260d1990
Lucas Duport <lucas.duport@epita.fr>
2023-12-08 09:39:34 +0100

    deprecated-happiness: tag number 6

--- deprecated_happiness/req03.sql
+++ deprecated_happiness/req03.sql
@@ -1,3 +1,4 @@
 ALTER TABLE migration.companies ADD growth numeric(6,2);
-ALTER TABLE migration.companies ADD CHECK (growth <= 1000 AND growth >= 0.0 AND NOT NULL);
+ALTER TABLE migration.companies ADD CONSTRAINT companies_growth_check
+CHECK (growth <= 1000 AND growth >= 0.0 AND NOT NULL);
 ALTER TABLE migration.companies ALTER COLUMN growth SET DEFAULT 0;
--- deprecated_happiness/req04.sql
+++ deprecated_happiness/req04.sql
@@ -2,8 +2,7 @@ ALTER TABLE migration.patients_records ADD COLUMN person_id integer;
 ALTER TABLE migration.patients_records
 ADD FOREIGN KEY (person_id) REFERENCES public.people(id);
 
-INSERT INTO migration.patients_records
-SELECT DISTINCT pi.person_id
+UPDATE migration.patients_records AS par
+SET person_id = pi.person_id
 FROM medical_confidential.patients_ids AS pi
-JOIN migration.patients_records AS pr
-ON pr.id = pi.patient_id;
+WHERE par.id = pi.patient_id;
